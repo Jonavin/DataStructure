af4726daf551abb80937514677b36ac8763355de
Jonavin <wangsongfeng@live.com>
2014-11-26 18:13:54 +0800

    code

--- queue/queue.js
+++ queue/queue.js
@@ -4,4 +4,18 @@
 ;
 (function (win){
     "use strict";
+    function Queue(){
+        this.datastore = [];//底层存储结构为数组
+        this.size = 0;//初始长度为0
+        this.front = null;//对头为空
+        this.rear = null;//队尾为空
+    }
+
+    /**
+     * 原型对象
+     */
+    Queue.prototype = {
+        __prop__:this.prototype,//原型引用
+        constructor:this//构造函数
+    }
 })(window);
