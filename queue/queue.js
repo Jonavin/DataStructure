c199cf29a3d9c462c212a5736283c7b0b52646de
Jonavin <wangsongfeng@live.com>
2014-11-27 09:59:49 +0800

    code

--- queue/queue.js
+++ queue/queue.js
@@ -9,13 +9,12 @@
         this.size = 0;//初始长度为0
         this.front = null;//对头为空
         this.rear = null;//队尾为空
+        this.constructor=0;//构造函数
     }
-
     /**
      * 原型对象
      */
     Queue.prototype = {
-        __prop__:this.prototype,//原型引用
-        constructor:this//构造函数
+        __prop__:this//原型引用
     }
 })(window);
